7f404bd7b00f1840a644e08d4d191d8980fd9e9a
Jake Kara <jake@jakekara.com>
2021-02-15 11:23:05 -0500

    Update typeahead to use flat JSON files

--- src/actions/typeahead.js
+++ src/actions/typeahead.js
@@ -1,34 +1,56 @@
 import fetch from 'isomorphic-fetch';
 
 export const setTypeaheadField = (field) => ({
-  type: 'SET_TYPEAHEAD_FIELD', field
-})
+  type: 'SET_TYPEAHEAD_FIELD',
+  field,
+});
 
 export const setTypeaheadQuery = (query) => ({
-  type: 'SET_TYPEAHEAD_QUERY', query
-})
+  type: 'SET_TYPEAHEAD_QUERY',
+  query,
+});
 
 export const setTypeaheadIndex = (index) => ({
-  type: 'SET_TYPEAHEAD_INDEX', index
-})
+  type: 'SET_TYPEAHEAD_INDEX',
+  index,
+});
 
 export const receiveTypeaheadResults = (results) => ({
-  type: 'RECEIVE_TYPEAHEAD_RESULTS', results
-})
+  type: 'RECEIVE_TYPEAHEAD_RESULTS',
+  results,
+});
 
 export const typeaheadRequestFailed = () => ({
-  type: 'TYPEAHEAD_REQUEST_FAILED'
-})
+  type: 'TYPEAHEAD_REQUEST_FAILED',
+});
 
 export function fetchTypeaheadResults(query) {
-  return function(dispatch) {
-    return fetch(query)
-      .then(response => response.json()
-        .then(json => ({ status: response.status, json })
-      ))
-      .then(({ status, json }) => {
-        if (status >= 400) dispatch(typeaheadRequestFailed())
-        else dispatch(receiveTypeaheadResults(json))
-      }, err => { dispatch(typeaheadRequestFailed()) })
-  }
+  // Determine whether we're querying authors or titles using the "field" param
+  const params = new URL(query).searchParams;
+  const field = params.get('field');
+  const term = params.get('value');
+  const rawDataPath = '/api/' + field + 's.json';
+
+  return function (dispatch) {
+    return fetch(rawDataPath)
+      .then((response) => {
+        if (response.status >= 400) dispatch(typeaheadRequestFailed());
+        return response.json();
+      })
+      .then((dataIndex) => {
+        const fullList = Object.keys(dataIndex);
+        const json = fullList.filter((v) =>
+          String(v).toLowerCase().includes(term.toLowerCase())
+        );
+        return { json };
+      })
+      .then(
+        ({ json }) => {
+          dispatch(receiveTypeaheadResults(json));
+        },
+        (err) => {
+          dispatch(typeaheadRequestFailed());
+        }
+      );
+  };
 }
