4ad550508b9162b033a8ebbee597b78d1c81d7a3
duhaime <douglas.duhaime@gmail.com>
2021-06-17 16:59:20 -0400

    remove unused

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import App from './components/App';
 import Results from './components/results/Results';
 import Waffle from './components/Waffle';
