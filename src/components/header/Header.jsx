d20e716e64e85206ae2f44a1f109994d821f64f1
duhaime <douglas.duhaime@gmail.com>
2021-07-12 15:16:41 -0400

    initial commit of header links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import brand from '../../assets/images/intertext.png';
+import { connect } from 'react-redux';
 
-const Header = (props) => (
-  <header>
-    <div className="header-text row align-center">
-      <Link className="brand" to="/">
-        <img src={brand} alt="Intertext brand logo" />
-      </Link>
-    </div>
-  </header>
-);
+const Header = (props) => {
+  const isActive = route => {
+    return props.route === route;
+  }
 
-export default Header;
+  return (
+    <header>
+      <div className="header-text row align-center space-between">
+        <Link className="brand" to="/">
+          <img src={brand} alt="Intertext brand logo" />
+        </Link>
+        <nav className='row align-center'>
+          {routes.map(r => (
+            <Link key={r.route} to={r.route} className={isActive(r.route) ? 'active' : ''}>
+              {r.label}
+            </Link>
+          ))}
+        </nav>
+        <div />
+      </div>
+    </header>
+  )
+}
+
+const routes = [
+  {
+    route: '/',
+    label: 'Cards',
+  },
+  {
+    route: '/sankey',
+    label: 'Sankey',
+  }
+]
+
+const mapStateToProps = state => ({
+  route: state.router.location.pathname,
+})
+
+export default connect(mapStateToProps)(Header);
