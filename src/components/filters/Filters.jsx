eb36fa3c4772680358080d3c2065a9d660f67fa4
duhaime <douglas.duhaime@gmail.com>
2021-07-23 05:50:20 -0400

    add alt tags

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -28,6 +28,7 @@ const Filters = (props) => {
           <ResultCount />
           <SortResults />
           <img
+            alt='Advanced filters icon'
             className={showAdvancedFilters ? 'active' : ''}
             id="advanced-filters-icon"
             src={filterIcon}
